4e1fead7429c8f39d40dc3cb1ff0749f9d135016
Bryant Plano <bryant@ionic.io>
2019-12-03 09:25:39 -0600

    Adding screen orientation plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,19 @@ export class HomePage {
 
   heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
 
-  constructor() {}
+  constructor(private screenOrientation: ScreenOrientation) {
+    this.screenOrientation.onChange().subscribe(
+      () => {
+          console.log("Orientation changed to: " + this.screenOrientation.type.toString);
+      }
+   );
+  }
 
+  lockLandscape() {
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+  }
+
+  unlockScreenOrientation(){
+    this.screenOrientation.unlock();
+  }
 }
